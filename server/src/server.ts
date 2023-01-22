a9c7119b2939159fb19ddada21a7eb2ee958a614
Duarte Martinho <dmartinho.personal@gmail.com>
2023-01-22 17:53:57 +0000

    vercel deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,11 @@ import Fastify from "fastify"
 import cors from "@fastify/cors"
 import { appRoutes } from "./routes"
 
+
+// Read the .env file.
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const app = Fastify()
 
 
@@ -9,8 +14,13 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-    port: 3333,
+    port: 3000,
     host: '0.0.0.0',
 }).then(() => {
-    // console.log('Server listening on port 3333')
+    console.log('Server listening on port 3333')
 })
+
+export default async (req: any, res: any) => {
+    await app.ready();
+    app.server.emit('request', req, res);
+}
